b876607f890fa4ca8749a8aa11e3aef462c7a72e
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-01-16 02:49:15 -0600

    Add hover state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,29 @@
 // Header.tsx
+import { useState } from "react";
 import "../styles/header.css";
-import giddyOSLogo from "../assets/img/giddyOSLogo.png";
+import giddyOSLogoBlack from "../assets/img/giddyOSLogoBlack.png";
+import giddyOSLogoWhite from "../assets/img/giddyOSLogoWhite.png";
 import TimeDisplay from "./TimeDisplay";
 import Dropdown from "./Dropdown";
 import Menu from "./Menu";
 import { MENUS, OS_MENU } from "../menus/menu.data";
 
 function Header() {
+  const [isLogoHovered, setIsLogoHovered] = useState(false);
   return (
     <header className="flex justify-between items-center px-4">
       <nav aria-label="Main menu">
-        <ul className="flex gap-4 items-center">
+        <ul className="flex gap-2 items-center">
           {/* OS Menu */}
-          <li>
+          <li
+            className={isLogoHovered ? "logo-hover" : ""}
+            onMouseEnter={() => setIsLogoHovered(true)}
+            onMouseLeave={() => setIsLogoHovered(false)}
+          >
             <Dropdown
               trigger={
                 <img
-                  src={giddyOSLogo}
+                  src={isLogoHovered ? giddyOSLogoWhite : giddyOSLogoBlack}
                   alt="giddyOS logo"
                   className="header-logo"
                 />
@@ -29,7 +36,7 @@ function Header() {
           {MENUS.map((menu) => (
             <li key={menu.label}>
               <Dropdown
-                trigger={<span className="menu-label">{menu.label}</span>}
+                trigger={<span className="p-1 menu-label">{menu.label}</span>}
                 content={<Menu items={menu.items} />}
               />
             </li>
