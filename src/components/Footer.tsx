1a5abedeef5168c31b938e7c83cba49f2482d061
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-01-19 15:34:34 -0600

    Add dock icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,27 @@
 import "../styles/footer.css";
+import finderIcon from "../assets/icons/finder.png";
+import chatIcon from "../assets/icons/chat.png";
+import browserIcon from "../assets/icons/browser.png";
+import trashIcon from "../assets/icons/trash.png"
 
 const FOOTER_APPS = [
-  { name: "Finder", icon: "" },
-  { name: "Chat", icon: "" },
-  { name: "Giddy Browser", icon: "" },
-  { name: "Giddy Mic", icon: "" },
+  { name: "Finder", icon: finderIcon },
+  { name: "Chat", icon: chatIcon },
+  { name: "Giddy Browser", icon: browserIcon },
+  { name: "Trash", icon: trashIcon },
 ];
 function Footer() {
   return (
     <footer className="p-4 gap-2">
       {FOOTER_APPS.map((footerApp) => {
-        return <div>{footerApp.name}</div>;
+        return <div key={footerApp.name} className="dock-icon">
+          <img
+            src={footerApp.icon}
+            alt={footerApp.name}
+            title={footerApp.name}
+            className="dock-icon-image"
+          />
+        </div>;
       })}
     </footer>
   );
