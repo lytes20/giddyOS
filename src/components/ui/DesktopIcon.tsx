efd0296fb70551dd4a5b179a3ff17d1384291fa2
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-01-21 17:10:09 -0600

    Add an onFocus

--- src/components/ui/DesktopIcon.tsx
+++ src/components/ui/DesktopIcon.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface ISystemApp {
   name: string;
   icon: string;
@@ -10,16 +12,40 @@ interface DesktopIconProps {
 function DesktopIcon(props: DesktopIconProps) {
   const { systemApp, onDoubleClick } = props;
   const { icon, name } = systemApp;
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleClick = () => {
+    // Click should also trigger focus for accessibility
+  };
+
+  const handleDoubleClick = () => {
+    onDoubleClick?.();
+  };
+
   return (
     <div
-      className="flex flex-col items-center w-fit cursor-pointer"
-      onDoubleClick={onDoubleClick}
+      className="flex flex-col items-center w-fit cursor-pointer focus:outline-none"
+      tabIndex={0}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onClick={handleClick}
+      onDoubleClick={handleDoubleClick}
     >
       <div className="w-[100px]">
         <img src={icon} className="w-full" />
       </div>
       <div>
-        <p>{name}</p>
+        <p className={isFocused ? "bg-black text-white px-1 rounded" : ""}>
+          {name}
+        </p>
       </div>
     </div>
   );
